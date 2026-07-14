3728b82796ad82002e511987297a06b6360df91d
Sapumal  Wijesingha <45715068+saputhere@users.noreply.github.com>
2026-07-15 02:11:47 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
-menuIcon.onclick=()=>{
+menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 }
@@ -14,24 +14,24 @@ let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
 
-    sections.forEach(sec=>{
-        let top=window.scrollY;
-        let offset=sec.offsetTop- 100;
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
         let height = sec.offsetHeight;
-        let id =sec.getAttribute('id');
+        let id = sec.getAttribute('id');
 
-        if(top >= offset && top <offset + height){
+        if (top >= offset && top < offset + height) {
             // active navbar links
-            navLinks.forEach(links =>{
+            navLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('header nav a[href*='+ id + ']').classList.add('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
             // active  sections for animation on scroll
             sec.classList.add('show-animate');
         }
         // if want to use animation that reapeates on scroll use this
 
-        else{
+        else {
             sec.classList.remove('show-animate')
         }
     });
@@ -42,23 +42,95 @@ window.onscroll = () => {
 
     header.classList.toggle('sticky', window.scrollY > 100);
 
- 
+
     // remove toggle icon and navbar when click navbar links (scroll)
-    
+
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 
     // animation footer on scroll
     let footer = document.querySelector('footer');
 
-    footer.classList.toggle('show-animate', this.innerHeight  + this.scrollY>=document.scrollingElement.scrollHeight);
+    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
 
 }
 
-        let popup = document.getElementById("popup");
-function openPopup(){
+let popup = document.getElementById("popup");
+function openPopup() {
     popup.classList.add("open-popup");
 }
-function closePopup(){
+function closePopup() {
     popup.classList.remove("open-popup");
 }
+
+
+
+// custome popup
+function openPopupinstagram() {
+    document.getElementById("customPopup").classList.add("active");
+}
+
+function closePopupinstagram() {
+    document.getElementById("customPopup").classList.remove("active");
+}
+
+window.onclick = function (e) {
+    const popup = document.getElementById("customPopup");
+
+    if (e.target === popup) {
+        closePopupinstagram();
+    }
+}
+
+
+const toggleBtn = document.getElementById("toggleSkills");
+const moreSkills = document.getElementById("moreSkills");
+
+toggleBtn.addEventListener("click", () => {
+
+    moreSkills.classList.toggle("show");
+    toggleBtn.classList.toggle("active");
+
+    if(moreSkills.classList.contains("show")){
+        toggleBtn.innerHTML =
+            "<i class='bx bx-chevron-up'></i> Show Less";
+    }else{
+        toggleBtn.innerHTML =
+            "<i class='bx bx-chevron-down'></i> See More Skills";
+    }
+
+});
+
+// Skill popup
+
+function openSkillsPopup() {
+    document.getElementById("skillsPopup").classList.add("show");
+}
+
+function closeSkillsPopup() {
+    document.getElementById("skillsPopup").classList.remove("show");
+}
+
+document.getElementById("skillsPopup").addEventListener("click", function (e) {
+    if (e.target === this) {
+        closeSkillsPopup();
+    }
+});
+
+// Projects popup
+
+    function openPopupProjects() {
+        document.getElementById("customPopupProject").style.display = "flex";
+    }
+
+    function closePopupProjects() {
+        document.getElementById("customPopupProject").style.display = "none";
+    }
+
+    // Close popup when clicking outside the popup box
+    window.onclick = function (event) {
+        const popup = document.getElementById("customPopupProject");
+        if (event.target === popup) {
+            closePopup();
+        }
+    };
